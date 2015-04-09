4dc08dd89d36bbc43e7136317a8b920e553f4038
Alexander Larsson <alexx.1992@gmail.com>
2015-04-09 12:09:20 +0200

    Awesome on table detection

--- app/src/main/java/com/example/iapps/timesapp/MainActivity.java
+++ app/src/main/java/com/example/iapps/timesapp/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.iapps.timesapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+    private TextView infoView;
+    private SensorManager mSensorManager;
+    private Sensor mGravity;
+
+    //All kod från: http://developer.android.com/guide/topics/sensors/sensors_overview.html
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoView = (TextView) findViewById(R.id.info_view);
+        infoView.setText("Not on table");
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null){
+            // Success! There's a gravity sensor.
+            mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        }
+        else {
+            // Failure! No gravity sensor.
+        }
     }
 
 
@@ -35,5 +58,40 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //Vaskad
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float axisz = event.values[2];
+        float sum = event.values[0] + event.values[1] + event.values[2];
+        float offset = 0.5f;
+
+        if(Math.abs(axisz - sum) < offset){
+            if(axisz > 0){
+                infoView.setText("On table");
+            }else{
+                infoView.setText("Upside down");
+            }
+
+        }else{
+            infoView.setText("Not on table");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
