77e15abaf437237f752a1d846182f807da8bd474
Alexander Larsson <alexx.1992@gmail.com>
2015-04-16 13:45:13 +0200

    Somthing is wrong...

--- app/src/main/java/com/example/iapps/timesapp/MainActivity.java
+++ app/src/main/java/com/example/iapps/timesapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,7 +14,6 @@ import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity implements SensorEventListener {
-    private TextView tableView;
     private SensorManager mSensorManager;
     private Sensor mGravity;
     private TextView minutesView;
@@ -21,13 +21,17 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
     private boolean onTable = false;
 
-    private int minutes = 0;
-    private int seconds = 0;
+    private long minutes = 0;
+    private long seconds = 0;
     private static final float NS2S = 1.0f / 1000000000.0f; //Convert nanoseconds to seconds
     private float timestamp = 0; // Last timestamp, needed for integration
     private float totalRotation = 0; //Total rotation so far
     float deltaRotation;
     private final static int DEGREES_PER_MINUTE = 5;
+    private final static int UPDATES_BEFORE_TIMER_START = 30; //About 2 sec
+    private int emptyUpdates = 0;
+
+    CountDownTimer coundDown = null;
 
 
     //All kod från: http://developer.android.com/guide/topics/sensors/sensors_overview.html
@@ -36,9 +40,6 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tableView = (TextView) findViewById(R.id.activity_main_rotation);
-        tableView.setText("Not on table");
         minutesView = (TextView) findViewById(R.id.activity_main_minutes);
         minutesView.setText("Not changed");
 
@@ -49,7 +50,6 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         }
         else {
             // Failure! No gravity sensor.
-            tableView.setText("No gravity sensor found");
         }
 
 
@@ -100,14 +100,12 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
             if (Math.abs(axisz - sum) < offset) {
                 if (axisz > 0) {
-                    tableView.setText("On table");
                     onTable = true;
                 } else {
-                    tableView.setText("Upside down");
+                    //Upside down
                 }
 
             } else {
-                tableView.setText("Not on table");
                 onTable = false;
             }
         } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
@@ -118,6 +116,11 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
                 if(event.values[2] > 0.1f || event.values[2] < -0.1f){
                     totalRotation += Math.toDegrees(deltaRotation);
+                    emptyUpdates = 0;
+                    if(coundDown != null){
+                        coundDown.cancel();
+                        coundDown = null;
+                    }
                     if(totalRotation > DEGREES_PER_MINUTE){
                         totalRotation = 0;
                         seconds = 0;
@@ -128,6 +131,26 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
                         seconds = 0;
                         minutes--;
                         updateTime();
+                    }
+                }else{
+                    emptyUpdates++;
+                    if(emptyUpdates > UPDATES_BEFORE_TIMER_START && minutes != 0 && seconds != 0){
+                        long millis = minutes * 60000;
+                        coundDown = new CountDownTimer(millis, 1000) {
+
+                            public void onTick(long millisUntilFinished) {
+                                long totalSeconds = millisUntilFinished / 1000;
+                                minutes = totalSeconds/60;
+                                seconds = totalSeconds%60;
+                                updateTime();
+                            }
+
+                            public void onFinish() {
+                                minutesView.setText("Done!");
+                                //Lägg till timer osv
+                            }
+                        };
+                        coundDown.start();
                     }
                 }
             }
@@ -140,13 +163,14 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
     private void updateTime(){
         minutesView.setText(String.valueOf(minutes + ":" + seconds));
+        //Lägg till tick ljud och sånt
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mGyro, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, mGyro, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
